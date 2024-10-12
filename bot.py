53441f0a041648648bf9ab63aef1a9a084ffde97
stashkiv-m <business.stashkiv@gmail.com>
2024-10-11 23:26:29 -0500

    test_commit

--- bot.py
+++ bot.py
@@ -29,7 +29,6 @@ def start(update: Update, context: CallbackContext) -> None:
 
 def main():
     """Start the bot."""
-    # test
     # Initialize the Updater with your bot's TOKEN
     updater = Updater(TOKEN, use_context=True)
 
