2a3644c288c59f63bcf865b66069230a34033d81
stashkiv-m <business.stashkiv@gmail.com>
2024-11-03 21:33:13 -0600

    time

--- bot.py
+++ bot.py
@@ -167,8 +167,8 @@ def main():
         signal_list_for_user(update, context)
 
     schedule_func_call(all_signals_calc_run, 22, 21)
-    schedule_func_call(send_daily_events(), 21, 32)
-    schedule_func_call(send_day_end_info(), 21, 33)
+    schedule_func_call(send_daily_events(), 21, 35)
+    schedule_func_call(send_day_end_info(), 21, 36)
 
     # Register command handlers
     dp.add_handler(CommandHandler("start", start))
